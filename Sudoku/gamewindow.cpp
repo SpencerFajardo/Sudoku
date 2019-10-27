20ea8ac8343ebcf2bedd7b63438f0632390ecd2c
Spencer Fajardo <spencer.fajardo@gmail.com>
2019-10-26 19:05:07 -0600

    I need to find an automated way to generate buttons

--- Sudoku/gamewindow.cpp
+++ Sudoku/gamewindow.cpp
@@ -19,6 +19,77 @@ GameWindow::~GameWindow()
 
 void GameWindow::on_Box1_textChanged()
 {
-    int data = ui->Box1->
-    emit boxDataChanged(0);
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(0,data);
+}
+
+void GameWindow::on_Box2_textChanged()
+{
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(1,data);
+}
+
+void GameWindow::on_Box3_textChanged()
+{
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(2,data);
+}
+
+void GameWindow::on_Box4_textChanged()
+{
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(3,data);
+}
+
+void GameWindow::on_Box5_textChanged()
+{
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(4,data);
+}
+
+void GameWindow::on_Box6_textChanged()
+{
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(5,data);
+}
+
+void GameWindow::on_Box7_textChanged()
+{
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(6,data);
+}
+
+void GameWindow::on_Box8_textChanged()
+{
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(7,data);
+}
+
+void GameWindow::on_Box9_textChanged()
+{
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(8,data);
+}
+
+void GameWindow::on_Box10_textChanged()
+{
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(9,data);
+}
+
+void GameWindow::on_Box11_textChanged()
+{
+    QString s = ui->textEdit->toPlainText();
+    int data = s.toInt();
+    emit boxDataChanged(10,data);
 }
